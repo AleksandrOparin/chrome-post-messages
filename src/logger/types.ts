32785aa20539fc2b725ccf9783b417f6228ef1be
Александр <al.oparin@vk.team>
2025-01-22 12:10:16 +0300

    Add tsc-alias and export for logger callback type

--- src/logger/types.ts
+++ src/logger/types.ts
@@ -1,6 +1,6 @@
 /** A callback signature for loggers. */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type LoggerCallback = (text: string, details?: any) => void;
+export type LoggerCallback = (text: string, details?: any) => void;
 
 /**
  * Represents a generic logger interface with different log levels.
